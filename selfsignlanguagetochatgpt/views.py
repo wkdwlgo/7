5e2bd500dac03ccefcfcfbbcb3ee0633961d1c0b
조승우 <kjsw12@gwnu.ac.kr>
2023-05-25 11:41:21 +0900

    change selfsign views

--- selfsignlanguagetochatgpt/views.py
+++ selfsignlanguagetochatgpt/views.py
@@ -55,7 +55,7 @@
 
 #             #result.result의 결과를 하나씩 chatGptPrompt에 추가한다.
 #             chatGptPrompt += result.result
-        
+
 #         #질문을 DB에 저장한다.
 #         chatResult = ChatResult()
 #         chatResult.prompt = chatGptPrompt
@@ -110,10 +110,12 @@ import cv2
 import string
 import mlflow
 import mlflow.keras
-from chatgpt.views import chatGPT
+from selfchatgpt.views import chatGPT
+
 logger = logging.getLogger('mylogger')
-#signlanguage/models.py의 Result 모델을 import한다.
+# signlanguage/models.py의 Result 모델을 import한다.
 from .models import ChatResult, Result
+
 # Create your views here.
 
 '''
@@ -122,37 +124,39 @@ from .models import ChatResult, Result
 3. 이때는 아래와 같이 사용한다.
  - 물론 이 부분도 view가 sql을 알면 안되서 분리해야 하지만, 짧은 교육상 이곳에 둔다. 
 '''
+
+
 def getChatResult(self, id):
-        query = "SELECT * FROM signlanguagetochatgpt_chatresult WHERE id = {0}".format(id)
-        logger.info(">>>>>>>> getChatResult SQL : "+query)
-        chatResult = self.t_exec(query)
+    query = "SELECT * FROM signlanguagetochatgpt_chatresult WHERE id = {0}".format(id)
+    logger.info(">>>>>>>> getChatResult SQL : " + query)
+    chatResult = self.t_exec(query)
+
 
 def index(request):
     return render(request, 'selflanguagechat/index.html')
 
+
 def chat(request):
     if request.method == 'POST' and request.FILES['files']:
 
-        results=[]
-        #form에서 전송한 파일을 획득한다.
-        #각 파일별 예측 결과들을 모아야 질문을 위한 언어가 완성된다.
+        results = []
+        # form에서 전송한 파일을 획득한다.
+        # 각 파일별 예측 결과들을 모아야 질문을 위한 언어가 완성된다.
         files = request.FILES.getlist('files')
         chatGptPrompt = ""
-        for idx,file in enumerate(files, start=0):
-                # files:
+        for idx, file in enumerate(files, start=0):
+            # files:
 
             # logger.error('file', file)
             # class names 준비
             class_names = list(string.ascii_lowercase)
             class_names = np.array(class_names)
 
-
             # mlflow 로딩
-            mlflow_uri="http://mini7-mlflow.carpediem.so/"
+            mlflow_uri = "http://mini7-mlflow.carpediem.so/"
             mlflow.set_tracking_uri(mlflow_uri)
-            model_uri = "models:/Sign_Signal/production"
+            model_uri = "models:/Sign_Signal_13/production"
             model = mlflow.keras.load_model(model_uri)
-
 
             # history 저장을 위해 객체에 담아서 DB에 저장한다.
             # 이때 파일시스템에 저장도 된다.
@@ -160,7 +164,6 @@ def chat(request):
             result.image = file
             result.pub_date = timezone.datetime.now()
             result.save()
-
 
             # 흑백으로 읽기
             img = cv2.imread(result.image.path, cv2.IMREAD_GRAYSCALE)
@@ -180,36 +183,32 @@ def chat(request):
 
             result_str = class_names[pred_1][0]
 
-
-            #결과를 DB에 저장한다.
+            # 결과를 DB에 저장한다.
             result.result = result_str
             # result.is_correct = 
             result.save()
             results.append(result)
 
-            #result.result의 결과를 하나씩 chatGptPrompt에 추가한다.
+            # result.result의 결과를 하나씩 chatGptPrompt에 추가한다.
             chatGptPrompt += result.result
-        
-        #질문을 DB에 저장한다.
+
+        # 질문을 DB에 저장한다.
         chatResult = ChatResult()
         chatResult.prompt = chatGptPrompt
         chatResult.pub_date = timezone.datetime.now()
         chatResult.save()
 
-
-        #저장된 질문을 DB에서 가져온다.
+        # 저장된 질문을 DB에서 가져온다.
         selectedChatResult = ChatResult.objects.get(id=chatResult.id)
 
-        #chatGptPrompt를 chatGPT에게 전달한다.
+        # chatGptPrompt를 chatGPT에게 전달한다.
         content = chatGPT(selectedChatResult.prompt)
         selectedChatResult.content = content
         selectedChatResult.save()
-        
-     
 
         context = {
-        'question': selectedChatResult.prompt,
-        'result': selectedChatResult.content
-    }
+            'question': selectedChatResult.prompt,
+            'result': selectedChatResult.content
+        }
 
-    return render(request, 'selflanguagechat/result.html', context)  
+    return render(request, 'selflanguagechat/result.html', context)
